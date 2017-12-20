c9fb033347ea0a2cb8f8c92296ac2f9606e83a03
Anastasiia-derymarko <lolololo2263@gmail.com>
2017-12-20 18:10:08 +0200

    Add files via upload

---
+++ слайдер/js.js
@@ -0,0 +1,27 @@
+var carousels = document.querySelectorAll('#carousels .carousel');
+  var currentCarousel = 0;
+  var carouselInterval = setInterval(nextCarousel,3000); /* Интервал между картинками */
+
+  function nextCarousel(){
+	carousels[currentCarousel].className = 'carousel';
+	currentCarousel = (currentCarousel+1)%carousels.length;
+	carousels[currentCarousel].className = 'carousel demonstration';
+	console.log(currentCarousel-1);
+	
+} 
+
+document.getElementById('right').onclick = rightCarousel;
+
+function rightCarousel(){
+	carousels[currentCarousel].className = 'carousel';
+	currentCarousel = (currentCarousel+1)%carousels.length;
+	carousels[currentCarousel].className = 'carousel demonstration';
+}
+
+document.getElementById('left').onclick = leftCarousel;
+
+function leftCarousel(){
+	carousels[currentCarousel].className = 'carousel';
+	currentCarousel = (currentCarousel+1)+carousels.length;
+	carousels[currentCarousel].className = 'carousel demonstration';
+}
